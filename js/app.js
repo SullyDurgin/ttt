29ea2a9e340ed34f2f514fcab4ad5611433d63da
SullyDurgin <katsdurgin@gmail.com>
2021-10-08 14:06:26 -0400

    logs id of sq when clicked

--- js/app.js
+++ js/app.js
@@ -15,23 +15,23 @@
 //   })
 // }
 
-  const checkWinIndexes = [
-		[0, 1, 2], // top row
-		[3, 4, 5], // middle row
-		[6, 7, 8], // bottom row
-		[0, 3, 6], // left column
-		[1, 4, 7], // middle column
-		[2, 5, 8], // right column
-		[0, 4, 8], // first diagonal
-		[2, 4, 6], // second diagonal
-	]
+  // const checkWinIndexes = [
+	// 	[0, 1, 2], // top row
+	// 	[3, 4, 5], // middle row
+	// 	[6, 7, 8], // bottom row
+	// 	[0, 3, 6], // left column
+	// 	[1, 4, 7], // middle column
+	// 	[2, 5, 8], // right column
+	// 	[0, 4, 8], // first diagonal
+	// 	[2, 4, 6], // second diagonal
+	// ]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let winners = new Array()
-let firstPlayerPicks = new Array()
-let secondPlayerPicks = new Array()
+// let winners = new Array()
+// let firstPlayerPicks = new Array()
+// let secondPlayerPicks = new Array()
 
 
 
@@ -49,6 +49,8 @@ let secondPlayerPicks = new Array()
 //use onload to start game when web page is load. But How?
 //onClick will mark select the right box, class = box
 
+
+
 function startGame() {
   document.currentPlayer = "X"  // at the start of the game X is the current Player
 }
@@ -56,13 +58,14 @@ function startGame() {
 function move(box) {
   if (box.innerText == '') // only mark the box if the str is empty first
   box.innerText = document.currentPlayer // mark box with X when clicked
-  nextPlayer()
+  //nextPlayer()
+  console.log(box)
 } 
 
-function nextPlayer() {
-	if (document.currentPlayer == 'X') {
-		document.currentPlayer = 'O'
-	} else {
-		document.currentPlayer = 'X'
-	}
-}
+// function nextPlayer() {
+// 	if (document.currentPlayer == 'X') {
+// 		document.currentPlayer = 'O'
+// 	} else {
+// 		document.currentPlayer = 'X'
+// 	}
+// }
