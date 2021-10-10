510e421607f978f5f274d15ebfaaa79742c48ae0
SullyDurgin <katsdurgin@gmail.com>
2021-10-10 19:11:56 -0400

    determin winner

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 
-  const winIndexes = [
+  const winningCombos = [
    [0, 1, 2], // top row
    [3, 4, 5], // middle row
    [6, 7, 8], // bottom row
@@ -8,19 +8,20 @@
    [1, 4, 7], // middle column
    [2, 5, 8], // right column
    [0, 4, 8], // first diagonal
-   [2, 4, 6], // second diagonal
+   [2, 4, 6], // secombo diagonal
 
   ]
 
-/*---------------------------- Variables (state) ----------------------------*/
+/*---------------------------- letiables (state) ----------------------------*/
 
-let board = [null, null, null, null, null, null, null, null, null]
-let clickedBoxes, currentPlayer, winner
+//let board = [null, null, null, null, null, null, null, null, null]
+let board, currentPlayer, winner
 
 /*------------------------ Cached Element References ------------------------*/
 
 const gameBoard = document.querySelector('.board')
 const boxes = document.querySelectorAll('.box')
+const gameStatus = document.querySelector('#message')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -34,33 +35,65 @@ boxes.forEach((box) => {
 init()
 
 function init() {
-  clickedBoxes = [null, null, null, null, null, null, null, null, null] // no player is yet on the board
+  board = [null, null, null, null, null, null, null, null, null] // no player is yet on the board
   currentPlayer = 1
   winner = null
   render()
 }
 
 function render() {
-  for (i = 0; i < clickedBoxes.length; i++) {
-    if (clickedBoxes[i] === 1) {
+  for (i = 0; i < board.length; i++) {
+    if (board[i] === 1) {
       boxes[i].innerText = 'X'
-    } else if (clickedBoxes[i] === -1) {
+    } else if (board[i] === -1) {
       boxes[i].innerText = 'O'
     } else {
       boxes[i].innerText = ''
     }
+    // if (winner === 1 || winner === -1) { //stop game after winner is declared
+    //   return
+    // }
+    if (winner !== null) {
+      updateWin(winner)
+      return
+    }
   }
 }
+
+function updateWin(winner) {
+  if (winner === 1) {
+    gameStatus.innerText = "X wins!"
+  } else if (winner === -1) {
+    gameStatus.innerText = 'O wins!'
+  }
+}
+
 
 function handleClick(event) {
   let i = parseInt(event.target.id[2]) // what is the id of the clicked box // I now know that parseInt needs a number only in order to work, that took forever 
-  if (clickedBoxes[i] !== null) {
+  if (board[i] !== null) {
   return
+  
   }
- 
-clickedBoxes[i] = currentPlayer
+board[i] = currentPlayer
 currentPlayer = currentPlayer * -1
 
-render() // render function does not need innerText of boxes, just update clickedBoxes & call render
+render() // render function does not need innerText of boxes, just update board & call render
+getWinner()
 }
 
+function getWinner() {
+	winningCombos.forEach(function (combo) {
+		let counter = 0
+		combo.forEach(function (num) {
+			counter += board[num]
+			if (Math.abs(counter) === 3) {
+				winner = counter / 3
+				return winner
+			} else {
+				winner === null
+				return winner
+			}
+		})
+	})
+}
